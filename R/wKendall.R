20ae3ceed956825930f11d8e19591afc179cf92b
Steven Chiou <schiou@utdallas.edu>
2018-07-15 16:42:03 -0500

    added wKendall for perturbation

--- R/wKendall.R
+++ R/wKendall.R
@@ -5,6 +5,9 @@
 #'
 #' @param trun left truncation time satisfying \code{trun} <= \code{obs}.
 #' @param obs observed failure time, must be the same length as \code{trun}, might be right-censored.
+#' @param delta an optional 0-1 vector of censoring indicator (0 = censored, 1 = event) for \code{obs}.
+#' If this vector is not specified, \code{condKendall} assumes no censoring and all observed failure time
+#' denote events.
 #' @param weights perturbation weights.
 #' 
 #' @export
