f86989e316631d47139d2ace9a6f7220b28182dd
Steven Chiou <schiou@utdallas.edu>
2019-10-30 11:23:29 -0500

    fixed some typos

--- inst/examples/ex_pmcc.R
+++ inst/examples/ex_pmcc.R
@@ -1,4 +1,3 @@
-## Generate simulated data from transformation model
 data(channing, package = "boot")
 chan <- subset(channing, sex == "Male" & entry < exit & cens == 1)
 with(chan, pmcc(entry, exit)) ## cannot handle right censored data
